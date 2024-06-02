39634c7a22aec7daca77d935b3534b70e8255014
Ivan <ivanr1520@gmail.com>
2024-06-01 17:22:03 -0700

    initial

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl">Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username} - {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
